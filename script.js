435311ec4578c23369c69b31d0c7cdeba3c4c309
kelvindubon-cpu <kelvindubon@gmail.com>
2026-03-02 11:52:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,24 +18,25 @@ papaScript.onload = () => {
         items.forEach(item => {
           results.innerHTML += `
             <div class="card">
-              <h2>${item.Cultivator}</h2>
-              <p>Split: ${item.Split}</p>
-              <p>Terpenes:</p>
-              <ul>
-                <li>DT1: ${item.DT1}</li>
-                <li>DT2: ${item.DT2}</li>
-                <li>DT3: ${item.DT3}</li>
-                <li>DT4: ${item.DT4}</li>
-                <li>DT5: ${item.DT5}</li>
-              </ul>
-              <p>QR: <a href="${item.QR}" target="_blank">${item.QR}</a></p>
-              <p>Ailments: ${item.Ailments}</p>
-              <p>Best For: ${item.BestFor}</p>
-              <p>Effects: ${item.Effects}</p>
-              <p>Smell: ${item.Smell}</p>
-              <p>Taste: ${item.Taste}</p>
-              <p>Lineage: ${item.Lineage}</p>
-            </div>
+  <h2>${item.Strain}</h2>
+  <p>Cultivator: ${item.Cultivator}</p>
+  <p>Split: ${item.Split}</p>
+  <p>Terpenes:</p>
+  <ul>
+    <li>DT1: ${item.DT1}</li>
+    <li>DT2: ${item.DT2}</li>
+    <li>DT3: ${item.DT3}</li>
+    <li>DT4: ${item.DT4}</li>
+    <li>DT5: ${item.DT5}</li>
+  </ul>
+  <p>QR: <a href="${item.QR}" target="_blank">${item.QR}</a></p>
+  <p>Ailments: ${item.Ailments}</p>
+  <p>Best For: ${item.BestFor}</p>
+  <p>Effects: ${item.Effects}</p>
+  <p>Smell: ${item.Smell}</p>
+  <p>Taste: ${item.Taste}</p>
+  <p>Lineage: ${item.Lineage}</p>
+</div>
           `;
         });
       }
@@ -51,3 +52,4 @@ papaScript.onload = () => {
       display(data);
     });
 };
+
